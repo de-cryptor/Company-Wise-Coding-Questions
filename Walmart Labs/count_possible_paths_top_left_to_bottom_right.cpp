74d239c655a890e39cd0fa2e2e8994a089dc4c4f
Unknown <bim2014001@iiita.ac.in>
2018-07-08 12:18:48 +0530

    Count all possible paths from top left to bottom right

---
+++ Walmart Labs/count_possible_paths_top_left_to_bottom_right.cpp
@@ -0,0 +1,39 @@
+/* 
+	@Author - Jatin Goel
+	@Institute - IIIT Allahabad
+	Hardwork definitely pays off. 
+	There is no substitute of hardwork.
+	There is no shortcut to success. 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int m,n;
+        long long MOD = 1e9 + 7;
+        cin >> m >> n;
+        long long dp[m+5][n+5];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<m;i++)
+        {
+            dp[i][0] = 1;
+        }
+        for(int i=0;i<n;i++)
+        {
+            dp[0][i] = 1;
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1])%MOD;
+            }
+        }
+        cout << dp[m-1][n-1] << endl;
+    }
+	return 0;
+}
